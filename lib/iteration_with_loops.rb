34df1fd2469994bcf8e48b8cb87caecae5e3d9d6
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-30 15:42:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,9 @@ row_index = 0
 while row_index < src.count do
   element_index = 0 
   while element_index < src[row_index].count do
-    src[row_index][element_index] % 2 == 0 
+    if src[row_index][element_index] % 2 == 0 
       even_value << src[row_index][element_index]
+    end
     element_index += 1
   end
   row_index += 1
